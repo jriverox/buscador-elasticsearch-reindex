22e61cc00862e5aebd2c76b4a4653d29bc38dfa0
hugoroca <hugo.rock20@hotmail.com>
2020-03-22 00:03:47 -0500

    pass server

--- src/server.js
+++ src/server.js
@@ -1,33 +1,55 @@
+const ElasticManager = require("./elasticManager");
 const Utils = require("./utils");
 const config = require("../config");
 
 module.exports = class {
-  async reindexar(elasticManager) {
-    const countries = elasticManager.COUNTRIES;
-    for (let i = 0; i < countries.length; i++) {
-      const country = countries[i];
+  constructor(clusterId) {
+    this.clusterId = clusterId;
+    this.elasticManager = new ElasticManager(clusterId);
+  }
+
+  async execJob() {
+    const countries = this.elasticManager.getCluster().COUNTRIES;
+    const indices = await elasticManager.catIndices("producto_v");
+    await this.evaluateTasksIncluster();
+    await Utils.asyncForEach(countries, async country => {
       const campaignList =
         config.CAMPAIGNS_BY_COUNTRIES[country] ||
         config.CAMPAIGNS_BY_COUNTRIES["DEFAULT"];
-      for (let j = 0; j < campaignList.length; j++) {
-        const campaign = campaignList[j];
-        const indexName = `${config.ELASTICSEARCH.INDEX_PATTERN}_${country}_${campaign}`;
+
+      for (let i = 0; i < config.PERSONALIZATION.length; i++) {
+        const personalization = config.PERSONALIZATION[i];
+        for (let j = 0; j < campaignList.length; j++) {
+          const campaign = campaignList[j];
+          const indexName = `${config.ELASTICSEARCH.INDEX_PATTERN}_${country}_${campaign}`;
+          const newIndexName = `${config.ELASTICSEARCH.NEW_INDEX_PATTERN}_${country}_${campaign}_${personalization.toLowerCase()}`;
+          const existIndex = indices.find(i => {
+            return i.index === indexName;
+          });
+          const existNewIndex = indices.find(i => {
+            return i.index === newIndexName;
+          });
+          if (existIndex && !existNewIndex) {
+
+          }
+        }
       }
-    }
+    });
   }
 
-  async getIndices(elasticManager) {
-    return await elasticManager.catIndices("producto_v");
-  }
+  async reindexar() {}
 
-  async evaluateTasksIncluster(elasticManager) {
+  async evaluateTasksIncluster() {
     try {
       const body = { query: { term: { completed: true } } };
-      const response = await elasticManager.search(config.NAME_INDEX_LOG, body);
+      const response = await this.elasticManager.search(
+        config.NAME_INDEX_LOG,
+        body
+      );
       await Utils.asyncForEach(response.hits.hits, async item => {
         const _source = item._source;
         const taskId = _source.taskId;
-        const taskComplete = await this.evaluateTaskId(elasticManager, taskId);
+        const taskComplete = await this.evaluateTaskId(taskId);
         await this.elasticManager.insertLog(
           _source.indexName,
           _source.startTime,
@@ -47,11 +69,11 @@ module.exports = class {
     }
   }
 
-  async evaluateTaskId(elasticManager, taskId) {
+  async evaluateTaskId(taskId) {
     let inWhile = true;
     let taskComplete = {};
     while (inWhile) {
-      let task = elasticManager.tasksGet(taskId);
+      let task = this.elasticManager.tasksGet(taskId);
       if (task.completed) {
         taskComplete = task;
         inWhile = task.completed;
