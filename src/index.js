79a18a8cdb3d6cee3668cefc441f21d751d6042b
Jhony Rivero <jhony.rivero@hotmail.com>
2020-03-23 15:22:24 -0500

    probando la ejecucion en paralelo con varios cluster

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+const argv = require('yargs')
+  .usage('Usage: $0 <command> [options]')
+  .command('cluster', 'numero del cluster')
+  .example('$0 cluster 1', 'cluster en el que se ejecutará la reindexación')
+  .alias('c', 'cluster')
+  .demandOption(['c']).argv;
+
 const Server = require("./server");
+const Utils = require("./utils");
 
 (async () => {
-  const clusterId = 1;
-  const server = new Server(clusterId);
-
-  await server.execJob();
+  const clusterId = argv.cluster;
+  console.log('cluster', clusterId);
+  //const server = new Server(clusterId);
+while(true) {
+  await Utils.backoff(2000);
+  console.log('cluster', clusterId, ' segs:', new Date().getSeconds());
+}
+  //await server.execJob();
 })();
